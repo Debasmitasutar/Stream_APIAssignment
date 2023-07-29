28ab518329a323e53f7711abd30ec144d84b091e
Debasmitasutar <debasmita12.ds@gmail.com>
2023-07-29 19:19:01 +0530

    Added StreamAssignment Program

--- StreamAssignment.java
+++ StreamAssignment.java
@@ -1,6 +1,13 @@
 package Streams;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 public class StreamAssignment {
     public void forEachElement(){
@@ -14,10 +21,19 @@ public class StreamAssignment {
                 .map(element -> element * 2)
                 .forEach(System.out::println);
     }
+    public void doubleValueNewlist(){
+        Object [] intArr = {7,8,9,48,9};
+        List<Object> doubleList = Arrays.stream(intArr)
+                                         .collect(Collectors.toList());
+        for (Object number : doubleList) {
+            System.out.println(number);
+        }
+    }
     public static void main (String[] args){
         StreamAssignment streamDemoObj = new StreamAssignment();
        // streamDemoObj.forEachElement();
-        streamDemoObj.doubleEachElement();
+       // streamDemoObj.doubleEachElement();
+        streamDemoObj.doubleValueNewlist();
 
     }
 }
